788be5d965bc7a67a985b59cc4f6fbdc0a34d841
zalin <saleemsiddique04@gmail.com>
2023-05-22 11:59:15 +0200

    TESTS VERIFICADOS

--- src/es/iesmz/ed/algoritmes/HyperParTest.java
+++ src/es/iesmz/ed/algoritmes/HyperParTest.java
@@ -1,10 +1,50 @@
 package es.iesmz.ed.algoritmes;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class HyperParTest {
+class HyperparTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void esHyperPar() {
+        boolean resultadoEsperado = true;
+        boolean resultadoReal = HyperPar.esHyperPar(2);
+        assertEquals(resultadoEsperado, resultadoReal);
+    }
+
+    @Test
+    void esHyperPar1() {
+        boolean resultadoEsperado = false;
+        boolean resultadoReal = HyperPar.esHyperPar(3);
+        assertEquals(resultadoEsperado, resultadoReal);
+    }
+
+    @Test
+    void esHyperPar2() {
+        boolean resultadoEsperado = true;
+        boolean resultadoReal = HyperPar.esHyperPar(2480);
+        assertEquals(resultadoEsperado, resultadoReal);
+    }
+
+    @Test
+    void esHyperPar3() {
+        boolean resultadoEsperado = false;
+        boolean resultadoReal = HyperPar.esHyperPar(3579);
+        assertEquals(resultadoEsperado, resultadoReal);
+    }
+
+    @Test
+    void esHyperPar4() {
+        boolean resultadoEsperado = false;
+        boolean resultadoReal = HyperPar.esHyperPar(00421421);
+        assertEquals(resultadoEsperado, resultadoReal);
+    }
+
+    @Test
+    void esHyperPar5() {
+        boolean resultadoEsperado = false;
+        boolean resultadoReal = HyperPar.esHyperPar(00424426);
+        assertEquals(resultadoEsperado, resultadoReal);
     }
 }
